bcf1351e6b60e829ae5c1f3c6f4ff6631601fbd8
squeezylt <git@pix.sh>
2023-02-20 16:02:30 -0500

    working category parser and writer

--- cat_parser.py
+++ cat_parser.py
@@ -1,7 +1,31 @@
+import configparser
+import os
 
 def writeCategory(mod_folder_path, level, label):
-    print(0)
+    config = configparser.ConfigParser()
+    path = os.path.join(mod_folder_path,'categories.cat')
+    config.read(path)
+    if not config.has_section("CATEGORIES"):
+        config.add_section("CATEGORIES")
+    config.set("CATEGORIES", "Level" + str(level), label)
+    
+    with open(path, 'w') as configfile:
+        config.write(configfile)
 
 def getCategories(mod_folder_path):
     categories = []
+    config = configparser.ConfigParser()
+    path = os.path.join(mod_folder_path,'categories.cat')
+    config.read(path)
+    
+    for each_section in config.sections():
+        for (each_key, each_val) in config.items(each_section):
+            categories.append(each_val)
     return categories
+
+def main():
+    #print, debug stuff
+    pass
+
+if __name__ == "__main__":
+    main()
